4e415b68e0a68d1afa59c945ee0c3d4e042314fd
vitalis13 <vitalis13@gmail.com>
2020-08-19 20:10:51 +0400

    срфchange Components

---
+++ src/Components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://c7.hotpng.com/preview/896/248/908/green-home-vkontakte-computer-icons-user-google-plus.jpg'></img>
+        </header>
+    )
+};
+export default Header;
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Profile = () => {
+  return(
+      <div className='content'>
+          <img src='https://img3.goodfon.ru/original/2880x1800/0/42/gory-ozero-priroda-nz-novaya.jpg'></img>
+          <div>ava + descript</div>
+          <div>New post</div>
+          <div>Post 1</div>
+          <div>Post 2</div>
+          <div>Post 3</div>
+
+
+
+      </div>
+
+  )
+};
+export default Profile;
