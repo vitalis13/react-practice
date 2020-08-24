79f9752f010c0acea1fed0f2db7d40c1f2c52779
vitalis13 <vitalis13@gmail.com>
2020-08-24 14:39:22 +0400

    addition Router, NavLink!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import './App.css';
-import Header from "./Components/Header";
-import Navbar from "./Components/Navbar";
-import Profile from "./Components/Profile";
-
-
+import Header from "./Components/Header/Header";
+import Navbar from "./Components/Navbar/Navbar";
+import ProfileModule from "./Components/Profile/Profile.module";
+import Dialogs from "./Components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
 
 
 function App() {
-    return(
-   <div className = 'app-wrapper'>
-       <Header/>
-       <Navbar/>
-       <Profile/>
+    return (
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Route path='/dialogs' component={Dialogs}/>
+                    <Route path='/Profile' component={ProfileModule}/>
+                </div>
+            </div>
+        </BrowserRouter>
 
 
-
-       </div>
-
-
-)}
+    )
+}
 
 
 
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import s from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
+console.log(s);
+
+
+const Navbar = () => {
+    return <nav className={s.nav}>
+        <div className={s.item}>
+            <NavLink to='/Profile' activeClassName={s.active}>Profile</NavLink>
+        </div>
+        <div className={`${s.item} ${s.active}`}>
+            <NavLink to='/Messages'>Messages</NavLink>
+        </div>
+        <div className={s.item}>
+            <a href='/News'>News</a>
+        </div>
+        <div className={s.item}>
+            <a>Music</a>
+        </div>
+        <div className={s.item}>
+            <a>Settings</a>
+        </div>
+    </nav>
+}
+
+export default Navbar;
